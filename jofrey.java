4f9ce46a7c7e98d9e27825083ab7ec5f7568e5a8
jofrey.onesmo <jofreyjofreyonesmo@gmail.com>
2025-04-05 18:09:23 +0300

    changes made

--- jofrey.java
+++ jofrey.java
@@ -1,8 +1,1 @@
-public class jofrey {
-    public static void main(String[] args) {
-        System .out.println("hellow jofrey");
-        String name ="hellow";
-        System.out.println("hellow");
 
-    }
-}
